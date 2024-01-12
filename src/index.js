28d8fd314494189bdf0a2c65dabc863c54315d8f
danica <danica.for.github@gmail.com>
2024-01-12 14:37:29 +0800

    start chapter3 Output Management

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
-import join from 'lodash/join'
-import './style.css'
-import Icon from './nian-nian.jpg'
-// import Data from './data.xml'
-import Notes from './data.csv'
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml); // 输出 `TOML Example`
-console.log(toml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(yaml); // 输出 `YAML Example`
-console.log(yaml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(json); // 输出 `JSON5 Example`
-console.log(json.owner.name); // 输出 `Tom Preston-Werner`
-
-
-
 function component () {
   const element = document.createElement('div')
   element.innerHTML = join(['Hello', 'webpack'], ' ')
-  element.classList.add('hello')
-
-  const myIcon = new Image()
-  myIcon.src = Icon
-
   element.appendChild(myIcon)
-
-  const Data = import( /* webpackChunkName: "my-chunk-name" */ './data.xml')
-  console.log(Data)
-  console.log(Notes)
-
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,55 +9,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    chunkFilename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-        //css-loader将css文件解析成JS字符串
-        //style-loader将JS字符串生成style标签插入文档
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-        //指定资源类型，webpack会根据文件大小自动在asset/resource和asset/inline之间选择
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|ttc)$/i,
-        type: 'asset/resource'
-        //成功解析资源，但字体样式不生效的时候，可以检查一下字体样式有没有写对
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader']
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader']
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse
-        }
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse
-        }
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse
-        }
-      }
-    ]
   }
 }
