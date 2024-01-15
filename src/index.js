45d842d2c62deff574a6abf81af7f61402573461
danica <xxxleoninexxx@gmail.com>
2024-01-15 11:52:44 +0800

    complate chapter5 code splitting and chapter17 lazy loading

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-function getComponent () {
-  return import(
-    /* webpackChunkName: "my-chunk-name" */
-    'lodash-es/join'
-  ).then(
-    ({ default: join }) => {
-      const element = document.createElement('div')
-      element.innerHTML = join(['Hello', 'webpack'], ' ')
-      return element
-    }
-  ).catch((error) => "An error occurred while loading the component")
-}
-
-getComponent().then((component) => {
-  document.body.appendChild(component)
-})
-
 async function getComponentAsync () {
   const element = document.createElement('div')
+  const button = document.createElement('button')
+  const br = document.createElement('br')
+
+  button.innerHTML = 'Click me and look at the console!'
+
   try {
     const { default: join } = await import('lodash-es/join')
     element.innerHTML = join(['Hello', 'webpack'], ' ')
   } catch (e) {
     return "An error occurred while loading the component"
+  }
+
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = async e => {
+    const res = await import('./print')
+    const { default: print } = res
+    print()
   }
   return element
 }
